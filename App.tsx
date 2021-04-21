79e04ad82db0aff6ac69f909ca64db3ca8336733
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-04-21 15:10:00 -0300

    Adicionado as telas UserIdentification e Confirmation, navegação entre telas e refatoração da tela Welcome

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcome';
+import AppLoading from 'expo-app-loading';
+
+import Routes from './src/routes';
+
+import { 
+  useFonts, 
+  Jost_400Regular,
+  Jost_600SemiBold 
+} from '@expo-google-fonts/jost';
 
 export default function App() {
+
+  const [ fontsLoades ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoades)
+    return <AppLoading />
+
   return (
-    <Welcome />
+    <Routes />
   )
 }
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,38 +1,58 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import {
+    SafeAreaView, 
+    View,
+    Text, 
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import { Button } from '../components/Button';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
-    const [visible, setVisible] = useState(false);
+    const navigation = useNavigation();
 
-    function handleVisibility() {
-        setVisible(true)
+    function handleStart() {
+        navigation.navigate('UserIndetification')
     }
 
-
-    return(
+    return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'}
+                    forma fácil
+                </Text>
 
-            {
-                visible &&
-                <Image source={wateringImg} style={styles.image} />
-            }
+                <Image
+                    source={wateringImg}
+                    style={styles.image}
+                    resizeMode='contain'
+                />
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. 
-                Nós cuidamos de lembrar sempre que precisar
-            </Text>
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas.
+                    Nós cuidamos de lembrar sempre que precisar
+                </Text>
 
-            <Button title=">" />
-
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.7}
+                    onPress={handleStart}
+                >
+                    <Feather
+                        name='chevron-right'
+                        style={styles.buttonIcon}
+                    />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -40,21 +60,31 @@ export function Welcome() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    image: {
+        height: Dimensions.get('window').width * 0.7
     },
     button: {
         backgroundColor: colors.green,
@@ -63,13 +93,9 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         marginBottom: 10,
         height: 56,
-        paddingHorizontal: 10
+        width: 56
     },
-    image: {
-        width: 292,
-        height: 284
-    },
-    buttonText: {
+    buttonIcon: {
         color: colors.white,
         fontSize: 24
     }
